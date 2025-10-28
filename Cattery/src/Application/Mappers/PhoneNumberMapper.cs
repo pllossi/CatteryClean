a7589aff0ea4c45bf6fcf7c60658251ae9187477
pllossi <pllossi@PC4610>
2025-10-28 11:35:50 +0100

    Phone Number mapper

---
+++ Cattery/src/Application/Mappers/PhoneNumberMapper.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTO;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class PhoneNumberMapper
+    {
+        public static PhoneNumberDTO ToDTO (this PhoneNumber phoneNumber)
+        {
+            return new PhoneNumberDTO(
+                phoneNumber.Value
+            );
+        }
+        public static PhoneNumber ToEntity(this PhoneNumberDTO phoneNumberDto)
+        {
+            return new PhoneNumber(
+                phoneNumberDto.PhoneNumber
+            );
+        }
+    }
+}
