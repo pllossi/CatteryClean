6a2206127d08662677b90d880fb55f0d597bf9e9
pllossi <pllossi@PC4610>
2025-10-28 11:35:33 +0100

    Cap mapper

---
+++ Cattery/src/Application/Mappers/CapMapper.cs
@@ -0,0 +1,26 @@
+﻿using Application.DTO;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class CapMapper
+    {
+        public static CapDTO ToDTO(this Cap cap)
+        {
+            return new CapDTO(
+                cap.Value
+            );
+        }
+        public static Cap ToEntity(this CapDTO capDto)
+        {
+            return new Cap(
+                capDto.Cap
+            );
+        }
+    }
+}
