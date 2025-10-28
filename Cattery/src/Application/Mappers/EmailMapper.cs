6366da63ed7a2b90a0a80efb6c411482512a8c3f
pllossi <pllossi@PC4610>
2025-10-28 11:35:40 +0100

    Email mapper

---
+++ Cattery/src/Application/Mappers/EmailMapper.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTO;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class EmailMapper
+    {
+        public static EmailDTO ToDTO(this Email email)
+        {
+            if (email == null) return null;
+            return new EmailDTO
+            (
+               email.Value
+            );
+        }
+    }
+}
