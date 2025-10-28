fb0060c4956080a2e3099e32f160c6dd5549f6a2
pllossi <pllossi@PC4610>
2025-10-28 11:39:17 +0100

    Adoption mapper

---
+++ Cattery/src/Application/Mappers/AdoptionMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTO;
+using Domain.Entities;
+
+namespace Application.Mappers
+{
+    public static class AdoptionMapper
+    {
+        public static AdoptionDTO ToDTO(this Adoption adoption)
+        {
+            return new AdoptionDTO(
+                adoption.Cat.ToDTO(),
+                adoption.Adopter.ToDTO(),
+                adoption.AdoptionDate
+            );
+        }
+        public static Adoption ToEntity(this AdoptionDTO adoptionDto)
+        {
+            return new Adoption(
+                adoptionDto.Adopter.ToEntity(),
+                adoptionDto.Cat.ToEntity(),
+                adoptionDto.AdoptionDate
+            );
+        }
+    }
+}
