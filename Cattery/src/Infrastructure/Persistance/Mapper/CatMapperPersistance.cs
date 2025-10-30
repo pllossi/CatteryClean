a48dab883c2270764f50ad1f6f903a01232b5402
pllossi <pllossi@PC4610>
2025-10-30 10:49:14 +0100

    Cat mapper persistance

---
+++ Cattery/src/Infrastructure/Persistance/Mapper/CatMapperPersistance.cs
@@ -0,0 +1,41 @@
+using System;
+using Domain.Entities;
+using Infrastructure.Persistance.DTO;
+
+namespace Infrastructure.Persistance.Mapper
+{
+    public static class CatMapperPersistance
+    {
+        public static CatDtoPersistance ToPersistanceDto(this Cat cat)
+        {
+            if (cat is null) throw new ArgumentNullException(nameof(cat));
+
+            return new CatDtoPersistance(
+                cat.Name,
+                cat.Breed,
+                cat.Male,
+                cat.Description,
+                cat.ExitDate,
+                cat.BirthDate,
+                cat.CodeId
+            );
+        }
+
+        public static Cat ToDomain(this CatDtoPersistance dto)
+        {
+            if (dto is null) throw new ArgumentNullException(nameof(dto));
+
+            var cat = new Cat(
+                dto.Name,
+                dto.Breed,
+                dto.Male,
+                dto.Description,
+                dto.ExitDate,
+                dto.BirthDate
+            );
+
+            cat.CodeId = dto.CodeId;
+            return cat;
+        }
+    }
+}
