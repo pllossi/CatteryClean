bfb4bccf1c65b1d480e0e759faa5cf3ec68f11c1
pllossi <pllossi@PC4610>
2025-10-30 10:47:41 +0100

    Adoption dto persistance

---
+++ Cattery/src/Infrastructure/Persistance/DTO/AdoptionDtoPersistance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.DTO
+{
+    public record AdoptionDtoPersistance
+        (
+        CatDtoPersistance Cat,
+        AdopterDtoPersistance Adopter,
+        DateTime AdoptionDate
+        );    
+}
