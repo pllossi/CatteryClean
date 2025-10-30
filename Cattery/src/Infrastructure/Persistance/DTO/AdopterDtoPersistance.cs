b12753fbdce57a5219f6d08a9069c202f8eefa65
pllossi <pllossi@PC4610>
2025-10-30 10:46:59 +0100

    Adopter dto persistance

---
+++ Cattery/src/Infrastructure/Persistance/DTO/AdopterDtoPersistance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTO;
+
+namespace Infrastructure.Persistance.DTO
+{
+    public record AdopterDtoPersistance(
+        string Name,
+        string Surname,
+        PhoneNumberDtoPersistance PhoneNumber,
+        EmailDtoPersistance Email,
+        string Address,
+        CapDtoPersistance Cap,
+        TaxIdDtoPersistance TaxId
+        );
+}
